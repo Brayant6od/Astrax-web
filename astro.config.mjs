034ae70bd3f7237dd8d1f8127955361760b67244
Brayant Montero <bmbrayant@gmail.com>
2026-06-02 01:09:13 -0600

    Fix: Base path para corregir carga de CSS en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
+  // 'site' es la dirección base de tu página en GitHub
+  site: 'https://brayant6od.github.io', 
+  
+  // 'base' es el nombre exacto de tu repositorio. 
+  // ¡Ojo! Debe empezar y terminar con diagonal (/)
+  base: '/Astrax-web/',                 
+  
   integrations: [tailwind()]
 });
